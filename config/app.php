65be246dfef777f65ed4236b8942420bffa443f0
mohsin-alt <72786184+mohsin-alt@users.noreply.github.com>
2021-02-07 01:15:06 +0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'https://absolute-i6eab.ondigitalocean.app'),
 
     'asset_url' => env('ASSET_URL', null),
 
